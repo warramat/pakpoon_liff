5beb5896f0da47b2b64b0b0304fd6f42b864989a
warramat <warramat1999@gmail.com>
2022-03-11 10:34:41 +0700

    เพิ่มข้อมู,ตรวจสอบ

--- js/custom.js
+++ js/custom.js
@@ -83,7 +83,7 @@ $(function () {
     jQuery(document).ready(function () {
       $('#home').backstretch(
         ['images/home-bg-slideshow1.jpg', 'images/home-bg-slideshow2.jpg'],
-        { duration: 2000, fade: 750 }
+        { duration: 3000, fade: 500 }
       );
     });
   });
