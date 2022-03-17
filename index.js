9fc3634a2e7daf82eb6f84833ac5a63b8ce59277
warramat <warramat1999@gmail.com>
2022-03-17 22:45:44 +0700

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-liff.init({ liffId: '1656902981-0g1VVnpN' });
-window.onload = () => {
-  if (!liff.isLoggedIn()) {
-    liff.login({
-      redirectUri: 'https://wonderful-lalande-320066.netlify.app/register.html'
-    });
-  }
-};
+
--- js/custom.js
+++ js/custom.js
@@ -1,107 +1,96 @@
-
 // ISOTOPE FILTER
-jQuery(document).ready(function($){
+liff.init({ liffId: '1656902981-0g1VVnpN' });
+jQuery(document).ready(function ($) {
+  if (!liff.isLoggedIn()) {
+    liff.login({
+      redirectUri: 'https://wonderful-lalande-320066.netlify.app/register.html'
+    });
+  }
+  if ($('.iso-box-wrapper').length > 0) {
+    var $container = $('.iso-box-wrapper'),
+      $imgs = $('.iso-box img');
 
-  if ( $('.iso-box-wrapper').length > 0 ) { 
-
-      var $container  = $('.iso-box-wrapper'), 
-        $imgs     = $('.iso-box img');
-
-      $container.imagesLoaded(function () {
-
-        $container.isotope({
+    $container.imagesLoaded(function () {
+      $container.isotope({
         layoutMode: 'fitRows',
         itemSelector: '.iso-box'
-        });
-
-        $imgs.load(function(){
-          $container.isotope('reLayout');
-        })
-
       });
 
-      //filter items on button click
+      $imgs.load(function () {
+        $container.isotope('reLayout');
+      });
+    });
 
-      $('.filter-wrapper li a').click(function(){
+    //filter items on button click
 
-          var $this = $(this), filterValue = $this.attr('data-filter');
+    $('.filter-wrapper li a').click(function () {
+      var $this = $(this),
+        filterValue = $this.attr('data-filter');
 
-      $container.isotope({ 
+      $container.isotope({
         filter: filterValue,
-        animationOptions: { 
-            duration: 750, 
-            easing: 'linear', 
-            queue: false, 
-        }                
-      });             
+        animationOptions: {
+          duration: 750,
+          easing: 'linear',
+          queue: false
+        }
+      });
 
-      // don't proceed if already selected 
+      // don't proceed if already selected
 
-      if ( $this.hasClass('selected') ) { 
-        return false; 
+      if ($this.hasClass('selected')) {
+        return false;
       }
 
       var filter_wrapper = $this.closest('.filter-wrapper');
       filter_wrapper.find('.selected').removeClass('selected');
       $this.addClass('selected');
 
-        return false;
-      }); 
-
+      return false;
+    });
   }
-
 });
-
 
 // PRELOADER JS
-$(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+$(window).load(function () {
+  $('.preloader').fadeOut(1000); // set duration in brackets
 });
-
 
 // jQuery to collapse the navbar on scroll //
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+$(window).scroll(function () {
+  if ($('.navbar').offset().top > 50) {
+    $('.navbar-fixed-top').addClass('top-nav-collapse');
+  } else {
+    $('.navbar-fixed-top').removeClass('top-nav-collapse');
+  }
 });
-
 
 /* HTML document is loaded. DOM is ready. 
 -------------------------------------------*/
-$(function(){
-
+$(function () {
   // ------- WOW ANIMATED ------ //
-  wow = new WOW(
-  {
+  wow = new WOW({
     mobile: false
   });
   wow.init();
 
-
   // HIDE MOBILE MENU AFTER CLIKING ON A LINK
-  $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-
+  $('.navbar-collapse a').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  });
 
   // NIVO LIGHTBOX
   $('.iso-box-section a').nivoLightbox({
-        effect: 'fadeScale',
-    });
-
+    effect: 'fadeScale'
+  });
 
   // HOME BACKGROUND SLIDESHOW
-  $(function(){
-    jQuery(document).ready(function() {
-    $('#home').backstretch([
-       "images/home-bg-slideshow1.jpg", 
-       "images/home-bg-slideshow2.jpg",
-        ],  {duration: 2000, fade: 750});
+  $(function () {
+    jQuery(document).ready(function () {
+      $('#home').backstretch(
+        ['images/home-bg-slideshow1.jpg', 'images/home-bg-slideshow2.jpg'],
+        { duration: 2000, fade: 750 }
+      );
     });
-  })
-
+  });
 });
-
