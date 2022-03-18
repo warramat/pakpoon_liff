a6065b9208965be1954ee4543d7ff569b2bf1c26
warramat <warramat1999@gmail.com>
2022-03-18 15:21:32 +0700

    first commit

--- register.js
+++ register.js
@@ -1,4 +1,5 @@
 liff.init({ liffId: '1656902981-0g1VVnpN' });
+
 var ThaiAPI = 'https://thaiaddressapi-thaikub.herokuapp.com/v1/';
 async function getProvinces() {
   let data = await fetch(`${ThaiAPI}thailand/provinces`);
