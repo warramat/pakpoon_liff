c94c51fe83d658af068d8d5f2914a2c7e72913de
warramat <warramat1999@gmail.com>
2022-03-21 10:16:17 +0700

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ liff.init({ liffId: '1656902981-0g1VVnpN' });
 jQuery(document).ready(function ($) {
   if (!liff.isLoggedIn()) {
     liff.login({
-      redirectUri: 'https://wonderful-lalande-320066.netlify.app/register.html'
+      redirectUri: 'https://pakpoon-city.netlify.app/register.html'
     });
   }
 });
