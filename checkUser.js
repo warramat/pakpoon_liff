ebe66a45384c53f950f4647afe9ea3200ba7cfd8
warramat <warramat1999@gmail.com>
2022-03-22 15:55:46 +0700

    first test

--- checkUser.js
+++ checkUser.js
@@ -1,8 +1,6 @@
-const urlParams = new URLSearchParams(window.location.search);
-const myParam = urlParams.get('userId');
 let baseURL =
   `https://smartcity-pakpoon-api.herokuapp.com/userSmart/user/searchUserID/?userID=` +
-  myParam;
+  userId;
 async function checkUser(userId) {
   const res = await fetch(`${baseURL}?=${userId}`);
   return res.ok;
