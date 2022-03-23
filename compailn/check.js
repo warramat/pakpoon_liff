938258c099d6cebd09c2b37d9e639a81c121fd90
warramat <warramat1999@gmail.com>
2022-03-23 09:23:46 +0700

    first test

--- compailn/check.js
+++ compailn/check.js
@@ -20,7 +20,7 @@ window.onload = async () => {
     document.getElementsByTagName('BODY')[0].style.display = '';
   }
   let data = await fetch(
-    'https://smartcity-pakpoon.herokuapp.com/appeal/appealAll'
+    'https://smartcity-pakpoon-api.herokuapp.com/userSmart/data/pagination?province=นครศรีธรรมราช&district=&sub_district'
   );
   let html = '';
   data = await data.json();
@@ -37,7 +37,7 @@ window.onload = async () => {
         </div>
         <div class="col-8">
           <h2>เลขที่ :${key + 1}</h2>
-          <h4>เรื่อง :${item.topic}</h4>
+          <h4>เรื่อง :${item.total}</h4>
           <h4>รายละเอียด :${item.type}</h4>
           <h4>สถานะ :<span style="color:${
             item.status === 'รอตรวจสอบ' ? 'skyblue' : 'pink'
