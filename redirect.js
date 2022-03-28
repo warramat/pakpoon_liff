fbab45781eddb7e20aa288764fde24081853411d
warramat <warramat1999@gmail.com>
2022-03-28 10:07:34 +0700

    first test

--- redirect.js
+++ redirect.js
@@ -5,14 +5,13 @@ async function getUID() {
   return uid;
 }
 async function redirectPage(page = '') {
-  if (liff.getOS() === 'web') {
     if (!liff.isLoggedIn()) {
       liff.login({
         redirectUri:
           'https://wonderful-lalande-320066.netlify.app/register.html'
       });
     }
-  }
+  
   const uid = await getUID();
   const ck = await checkUser(uid);
   if (!ck) {
