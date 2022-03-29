c0b392d49bc60d09ba798d8a497ac5c528c86189
warramat <warramat1999@gmail.com>
2022-03-29 16:39:06 +0700

    first test

--- compailn/check.js
+++ compailn/check.js
@@ -6,7 +6,7 @@ window.onload = async () => {
   let html = '';
   data = await data.json();
 
-  data.map((item, key) => {
+  data.forEach((item, key) => {
     html += `
       <div class="min-vw-100">
         <div class="card">
