653fd87abc2f4a5b8f9af3011507c194c58407bf
warramat <warramat1999@gmail.com>
2022-03-31 10:20:30 +0700

    first test

--- health/health_Edit.js
+++ health/health_Edit.js
@@ -82,6 +82,10 @@ $('#waistline').change(() => {
   }
 });
 
+$('#year').change(() => {
+  makemouth();
+});
+
 function set() {
   const BMI = $('#BMI').val();
   const proportion = $('#proportion').val();
@@ -98,17 +102,48 @@ function set() {
   }
 }
 
-
-$( document ).ready(function() {
-   makeyear()
+$(document).ready(function () {
+  makeyear();
+  makemouth();
 });
 
-function makeyear(){
+function makeyear() {
   let today = new Date();
-  const year = today.getFullYear()+543
-  let html=''
-  for(year;year>2449;year--){
-      html+=`  <option value="${year}">${year}</option>`
+  const year = today.getFullYear() + 543;
+  let html = '';
+  for (year; year > 2449; year--) {
+    html += `  <option value="${year}">${year}</option>`;
   }
-  $('#year').html(html)
+  $('#year').html(html);
+}
+
+function makemouth() {
+  let today = new Date();
+  let html = '';
+  const M = [
+    'มกราคม',
+    'กุมภาพันธ์',
+    'มีนาคม',
+    'เมษายน',
+    'พฤษภาคม',
+    'มิถุนายน',
+    'สิงหาคม',
+    'กันยายน',
+    'ตุลาคม',
+    'พฤศจิกายน',
+    'ธันวาคม'
+  ];
+  if ($('#year').val() == today.getFullYear() + 543) {
+    M.forEach((e, i) => {
+      if (i > today.getMonth()) {
+        return;
+      }
+      html += `  <option value="${e}">${e}</option>`;
+    });
+  } else {
+    M.forEach((e, i) => {
+      html += `  <option value="${e}">${e}</option>`;
+    });
+  }
+  $('#month').html(html);
 }
