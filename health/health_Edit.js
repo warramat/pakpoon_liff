e2084ac2b3328c7676e65d03d7a81e00581b19a0
warramat <warramat1999@gmail.com>
2022-03-31 10:53:00 +0700

    first test

--- health/health_Edit.js
+++ health/health_Edit.js
@@ -90,21 +90,24 @@ function renderday(t) {
   if (leapyear(Number($('#year').val()) - 543)) {
     if ($('#month').val() === 'กุมภาพันธ์') {
       for (let i = 1; i != 30; i++) {
-        if (t && today.getDate() > i) {
+        if (t && today.getDate() === i) {
+          html += `<option selected value="${i}">${i}</option>`;
           break;
         }
         html += `<option value="${i}">${i}</option>`;
       }
     } else if ($('#month').val().indexOf('ยน') != -1) {
       for (let i = 1; i != 31; i++) {
-        if (t && today.getDate() > i) {
+        if (t && today.getDate() === i) {
+          html += `<option selected value="${i}">${i}</option>`;
           break;
         }
         html += `<option value="${i}">${i}</option>`;
       }
     } else {
       for (let i = 1; i != 32; i++) {
-        if (t && today.getDate() > i) {
+        if (t && today.getDate() === i) {
+          html += `<option selected value="${i}">${i}</option>`;
           break;
         }
         html += `<option value="${i}">${i}</option>`;
@@ -113,21 +116,24 @@ function renderday(t) {
   } else {
     if ($('#month').val() === 'กุมภาพันธ์') {
       for (let i = 1; i != 29; i++) {
-        if (t && today.getDate() > i) {
+        if (t && today.getDate() === i) {
+          html += `<option selected value="${i}">${i}</option>`;
           break;
         }
         html += `<option value="${i}">${i}</option>`;
       }
     } else if ($('#month').val().indexOf('ยน') != -1) {
       for (let i = 1; i != 31; i++) {
-        if (t && today.getDate() > i) {
+        if (t && today.getDate() === i) {
+          html += `<option selected value="${i}">${i}</option>`;
           break;
         }
         html += `<option value="${i}">${i}</option>`;
       }
     } else {
       for (let i = 1; i != 32; i++) {
-        if (t && today.getDate() > i) {
+        if (t && today.getDate() === i) {
+          html += `<option selected  value="${i}">${i}</option>`;
           break;
         }
         html += `<option value="${i}">${i}</option>`;
