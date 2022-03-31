849e6a82d964b3d95c03e37062f8a2f4718bfa26
warramat <warramat1999@gmail.com>
2022-03-31 10:47:36 +0700

    first test

--- health/health_Edit.js
+++ health/health_Edit.js
@@ -50,3 +50,85 @@ function makemouth() {
   }
   $('#month').html(html);
 }
+
+function makeday() {
+  let today = new Date();
+  const M = [
+    'มกราคม',
+    'กุมภาพันธ์',
+    'มีนาคม',
+    'เมษายน',
+    'พฤษภาคม',
+    'มิถุนายน',
+    'สิงหาคม',
+    'กันยายน',
+    'ตุลาคม',
+    'พฤศจิกายน',
+    'ธันวาคม'
+  ];
+  if (
+    $('#year').val() == today.getFullYear() + 543 &&
+    today.getMonth() === M.indexOf($('#month').val())
+  ) {
+    renderday(true);
+  } else {
+    renderday(false);
+  }
+}
+
+function leapyear(year) {
+  return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+}
+
+function renderday(t) {
+  let today = new Date();
+  let html = '';
+  if (leapyear(Number($('#year').val()) - 543)) {
+    if ($('#month').val() === 'กุมภาพันธ์') {
+      for (let i = 1; i != 30; i++) {
+        if (t && today.getdate() > i) {
+          break;
+        }
+        html += `<option value="${i}">${i}</option>`;
+      }
+    } else if ($('#month').val().indexOf('ยน') != -1) {
+      for (let i = 1; i != 31; i++) {
+        if (t && today.getdate() > i) {
+          break;
+        }
+        html += `<option value="${i}">${i}</option>`;
+      }
+    } else {
+      for (let i = 1; i != 32; i++) {
+        if (t && today.getdate() > i) {
+          break;
+        }
+        html += `<option value="${i}">${i}</option>`;
+      }
+    }
+  } else {
+    if ($('#month').val() === 'กุมภาพันธ์') {
+      for (let i = 1; i != 29; i++) {
+        if (t && today.getdate() > i) {
+          break;
+        }
+        html += `<option value="${i}">${i}</option>`;
+      }
+    } else if ($('#month').val().indexOf('ยน') != -1) {
+      for (let i = 1; i != 31; i++) {
+        if (t && today.getdate() > i) {
+          break;
+        }
+        html += `<option value="${i}">${i}</option>`;
+      }
+    } else {
+      for (let i = 1; i != 32; i++) {
+        if (t && today.getdate() > i) {
+          break;
+        }
+        html += `<option value="${i}">${i}</option>`;
+      }
+    }
+  }
+  return $('#day').html(html);
+}
