664fb019268573df7f7043c43839dd38c59dc140
warramat <warramat1999@gmail.com>
2022-04-04 08:50:44 +0700

    first test

--- other/form.js
+++ other/form.js
@@ -12,7 +12,7 @@ $(document).ready(async () => {
   );
   data = await data.json();
   let html = '';
-  $('#topic').text(data.topic.type);
+  $('#topic').text(data.type);
   data.type.forEach((element) => {
     html += `<option value="${element}">${element}</option>`;
   });
