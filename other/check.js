b36d5385b674b8525ace9d46a2e2a72da5a089e9
warramat <warramat1999@gmail.com>
2022-04-04 10:43:54 +0700

    first test

--- other/check.js
+++ other/check.js
@@ -10,21 +10,24 @@ window.onload = async () => {
   data = data.reverse();
   data.forEach((item, key) => {
     html += `
-        <div class="min-vw-100">
-          <div class="card">
-            <div class="card-body">
-            <div class="col-12">
-            <h3 style="color:blue">เลขที่ :${key + 1}</h3>
-            <h4>เรื่อง :${item.topic}</h4>
-            <h4>สถานะ :<span style="color:${
-              item.status === 'รอตรวจสอบ' ? 'skyblue' : 'pink'
-            }";>${item.status}</span></h4>
-            <br />
-            <h4>${item.updated}</h4>
-          </div>
-            </div>
-          </div>
-        </div>
+    <a href="./Checkhealth.html?_id=${item._id}">
+    <div class="min-vw-100">
+    <div class="card">
+      <div class="card-body">
+      <div class="col-12">
+      <h3 style="color:blue">เลขที่ :${key + 1}</h3>
+      <h4>เรื่อง :${item.topic}</h4>
+      <h4>สถานะ :<span style="color:${
+        item.status === 'รอตรวจสอบ' ? 'skyblue' : 'pink'
+      }";>${item.status}</span></h4>
+      <br />
+      <h4>${item.updated}</h4>
+    </div>
+      </div>
+    </div>
+  </div>
+    </a>
+       
         `;
   });
   document.getElementById('check').innerHTML = html;
