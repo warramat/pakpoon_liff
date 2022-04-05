2d4ab4075d72ce6f4de260f736b523807fdcb0d4
warramat <warramat1999@gmail.com>
2022-04-05 11:35:49 +0700

    first test

--- compailn/check.js
+++ compailn/check.js
@@ -30,7 +30,7 @@ window.onload = async () => {
         <br />
         <a class="btn btn-outline-primary " href="./check_details?_id=${
           item._id
-        }&item=${key}" style="width: 100%;">
+        }&key=${key}" style="width: 100%;">
         ดูรายละเอียด</a>
       </div>
           </div>
