45968874e0bbbb3088756e53355133b9aad5cd63
warramat <warramat1999@gmail.com>
2022-04-05 11:41:09 +0700

    first test

--- compailn/check.js
+++ compailn/check.js
@@ -18,7 +18,7 @@ window.onload = async () => {
             item.img[0]
           }"/></h2>
         </div>
-        <div class="col-8">
+        <div class="col-8" style="font-size:20pt;">
           <h2 style="color:green">เลขที่ :${key + 1}</h2>
           <h4>เรื่อง :${item.topic}</h4>
           <h4>รายละเอียด :${item.type}</h4>
