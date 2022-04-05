40b14703992638af191f6787f934ce6f2673265c
warramat <warramat1999@gmail.com>
2022-04-05 12:06:04 +0700

    first test

--- compailn/check_details.js
+++ compailn/check_details.js
@@ -11,7 +11,7 @@ window.onload = async () => {
   data = data.find((e) => e._id === _id);
   let html_img = ''
   data.img.forEach(e => {
-    html_img+=`<img style="width:300px" src="https://smartcity-pakpoon-api.herokuapp.com/userSmart/${e}"/>` 
+    html_img+=`<img style="width:100px" src="https://smartcity-pakpoon-api.herokuapp.com/userSmart/${e}"/>` 
   });
   let html = `
     <div class="d-flex align-items-center" style="margin-top: 2rem; flex-direction: column;">
