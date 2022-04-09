7ee9fe80c1689094034920dd54a4a837cdd1f360
warramat <warramat1999@gmail.com>
2022-04-09 22:17:11 +0700

    first test

--- health/Checkhealth.js
+++ health/Checkhealth.js
@@ -7,10 +7,12 @@ liff.init({ liffId: '1656902981-0g1VVnpN' }).then(async () => {
         'https://tapp-smartcity.netlify.app/health/Checkhealth.html?_id=' +
         myParam
     });
-  } else if (!checkUser(await getUID())) {
-    location.replace('https://tapp-smartcity.netlify.app/register.html');
+  } else if (!(await checkUser(await getUID()))) {
+    window.location = '../register.html';
   } else if (!(await getFriend())) {
     window.location = 'https://line.me/R/ti/p/@172nwynm';
+  } else {
+    document.getElementById('show').style.visibility = 'visible';
   }
 });
 
--- other/form.js
+++ other/form.js
@@ -6,10 +6,12 @@ liff.init({ liffId: '1656902981-0g1VVnpN' }).then(async () => {
       redirectUri:
         'https://tapp-smartcity.netlify.app/other/form.html?topic=' + myParam
     });
-  } else if (!checkUser(await getUID())) {
-    location.replace('https://tapp-smartcity.netlify.app/register.html');
+  } else if (!(await checkUser(await getUID()))) {
+    window.location = '../register.html';
   } else if (!(await getFriend())) {
     window.location = 'https://line.me/R/ti/p/@172nwynm';
+  } else {
+    document.getElementById('show').style.visibility = 'visible';
   }
 });
 
