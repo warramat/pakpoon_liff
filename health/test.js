562266340157640f2a6c852df6a590d506b251e4
warramat <warramat1999@gmail.com>
2022-08-22 09:49:45 +0700

    first name

--- health/test.js
+++ health/test.js
@@ -8,7 +8,7 @@ liff.init({ liffId: '1656902981-0g1VVnpN' }).then(async () => {
   } else if (!(await getFriend())) {
     window.location = 'https://line.me/R/ti/p/@172nwynm';
   } else {
-    document.getElementById('form1').style.visibility = 'visible';
+    document.getElementById('show').style.visibility = 'visible';
   }
 });
 var proportion = '';
