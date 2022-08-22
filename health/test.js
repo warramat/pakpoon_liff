df7e1f23cac7c099b82f2fa085bcecdd01ec6963
warramat <warramat1999@gmail.com>
2022-08-22 09:29:11 +0700

    first name

--- health/test.js
+++ health/test.js
@@ -36,6 +36,8 @@ $('form').submit(function (e) {
     denyButtonText: 'ยกเลิก'
   }).then(async (result) => {
     if (result.isConfirmed) {
+      var UID = await getUID();
+      data.userID = UID;
       const form = $('form').serializeArray();
       const n = form.length;
       sum = 0;
