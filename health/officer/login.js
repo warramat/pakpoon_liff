f2c207b2e20222311c0504dcd461f00653b7ff92
warramat <warramat1999@gmail.com>
2022-09-07 09:12:40 +0700

    firstname

---
+++ health/officer/login.js
@@ -0,0 +1,45 @@
+var jwt = localStorage.getItem('jwt');
+if (jwt != null) {
+  window.location.href = './home_officer.html';
+}
+
+$('form').submit(async (e) => {
+  e.preventDefault();
+  const userid = document.getElementById('userid').value;
+  const password = document.getElementById('password').value;
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json;charset=UTF-8');
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: JSON.stringify({
+      userid: userid,
+      password: password
+    }),
+    redirect: 'follow'
+  };
+  const res = await fetch(
+    'https://smartcity-pakpoon-api.herokuapp.com/User-admin/Cre-employee',
+    requestOptions
+  );
+  if (res.status === 201) {
+    const data = await res.json();
+    localStorage.setItem('jwt', data.access_token);
+    Swal.fire({
+      text: 'เข้าสู่ระบบเรียบร้อย',
+      icon: 'success',
+      confirmButtonText: 'OK',
+      timer: 3000
+    }).then((result) => {
+      window.location.href = './home_officer.html';
+    });
+  } else {
+    const error = await res.json();
+    Swal.fire({
+      text: error.message,
+      icon: 'error',
+      confirmButtonText: 'OK',
+      timer: 3000
+    });
+  }
+});
