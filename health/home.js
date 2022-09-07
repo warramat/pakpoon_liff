7cf86bb7cd8d28cc337e6aec5516913a78f080a1
warramat <warramat1999@gmail.com>
2022-09-07 08:57:58 +0700

    firstname

---
+++ health/home.js
@@ -0,0 +1,24 @@
+liff.init({ liffId: '1656902981-0g1VVnpN' }).then(async () => {
+    if (!liff.isLoggedIn()) {
+      liff.login({
+        redirectUri: 'https://tapp-smartcity.netlify.app/health/home.html'
+      });
+    } else if (!(await checkUser(await getUID()))) {
+      window.location = '../register.html';
+    } else if (!(await getFriend())) {
+      window.location = 'https://line.me/R/ti/p/@172nwynm';
+    } else {
+      document.getElementById('show').style.visibility = 'visible';
+    }
+  });
+  
+  async function getFriend() {
+    const friend = await liff.getFriendship();
+    return friend.friendFlag;
+  }
+  async function getUID() {
+    const data = await liff.getProfile();
+    const uid = await data.userId;
+    return uid;
+  }
+  
