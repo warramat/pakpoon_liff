fba727391c466d0c5d14b83a915dd793e9ca37df
warramat <warramat1999@gmail.com>
2022-10-25 11:29:15 +0700

    map

--- compailn/appointment.js
+++ compailn/appointment.js
@@ -90,7 +90,7 @@ $('form').submit((e) => {
       };
       fetch(
         // 'https://smartcity-pakpoon.herokuapp.com/appeal/addappeal',
-        'https://smartcity-pakpoon-api.herokuapp.com/appeal/addappeal',
+        'ttps://smartcity-pakpoon-api.herokuapp.com/appeal/addappointment',
         requestOptions
       )
         .then(() => {
