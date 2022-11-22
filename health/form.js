ef48e0f1d61f609fed5dd148f8d8547b47dda310
warramat <warramat1999@gmail.com>
2022-11-22 22:45:35 +0700

    first commit

--- health/form.js
+++ health/form.js
@@ -1,55 +1,27 @@
+liff.init({ liffId: '1656902981-0g1VVnpN' }).then(async () => {
+  if (!liff.isLoggedIn()) {
+    liff.login({
+      redirectUri: 'https://tapp-smartcity.netlify.app/health/form.html'
+    });
+  } else if (!(await checkUser(await getUID()))) {
+    window.location = '../register.html';
+  } else if (!(await getFriend())) {
+    window.location = 'https://line.me/R/ti/p/@172nwynm';
+  } else {
+    document.getElementById('show').style.visibility = 'visible';
+  }
+});
+var proportion = '';
+async function getFriend() {
+  const friend = await liff.getFriendship();
+  return friend.friendFlag;
+}
+async function getUID() {
+  const data = await liff.getProfile();
+  const uid = await data.userId;
+  return uid;
+}
 
-//  liff.init({ liffId: '1656902981-0g1VVnpN' }).then(async () => {
-//      if (!liff.isLoggedIn()) {
-//        liff.login({
-//          redirectUri: 'https://tapp-smartcity.netlify.app/register.html'
-//        });
-//      } else if (await checkUser(await getUID())) {
-//        window.location = './index.html';
-//      } else if (!(await getFriend())) {
-//        window.location = 'https://line.me/R/ti/p/@172nwynm';
-//      } else {
-//        document.getElementById('show').style.visibility = 'visible';
-//      }
-//    });
-
-//    async function getFriend() {
-//      const friend = await liff.getFriendship();
-//      return friend.friendFlag;
-//    }
-
-//    async function getUID() {
-//      const data = await liff.getProfile();
-//      const uid = await data.userId;
-//      return uid;
-//    }
-
-// var form = document.getElementById('form-head')
-//   form.addEventListener('submit',function(e){
-//     e.preventDefault()
-//     var prefix = document.getElementById('prefix').value
-//     var name = document.getElementById('name').value
-//     var lastname = document.getElementById('lastname').value
-
-//     fetch('https://jsonplaceholder.typicode.com/posts', {
-//       method: 'POST',
-//       body:JSON.stringify({
-//         prefix: prefix,
-//         name:name,
-//         lastname:lastname
-//       }),
-//       headers:{
-//         'content-Type': 'application/json ;charset=UTF-8'
-//       }
-//     })
-//     .then(function(response){
-//       return response.json()
-//     })
-//     .then(function(data){
-//       console.log(data)
-//     })
-//   })
-  
 
 function Disable(c) {
   const a = $(`.${c}`);
@@ -201,7 +173,8 @@ $('#TWH01').submit(function (e) {
     denyButtonText: 'ยกเลิก'
   }).then(async (result) => {
     if (result.isConfirmed) {
-        let data = {}
+      var UID = await getUID();
+      sendData.userID = UID;
           const form = $('#TWH01').serializeArray();
           const n = form.length;
           sum = 0;
@@ -216,7 +189,7 @@ $('#TWH01').submit(function (e) {
         fetch(
           'https://smartcity-pakpoon-api.herokuapp.com/employee/addeemployee',
           {
-            method: 'POST',
+            method: 'PUT',
             body: JSON.stringify(sendData),
             headers: {
               'Content-Type': 'application/json; charset=UTF-8'
