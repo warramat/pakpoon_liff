fb10ef54013d8e52596fc1439fec7e688677c74c
warramat <warramat1999@gmail.com>
2022-11-22 22:52:30 +0700

    first commit

--- health/form.js
+++ health/form.js
@@ -1,26 +1,28 @@
-liff.init({ liffId: '1656902981-0g1VVnpN' }).then(async () => {
-  if (!liff.isLoggedIn()) {
-    liff.login({
-      redirectUri: 'https://tapp-smartcity.netlify.app/health/form.html'
-    });
-  } else if (!(await checkUser(await getUID()))) {
-    window.location = '../register.html';
-  } else if (!(await getFriend())) {
-    window.location = 'https://line.me/R/ti/p/@172nwynm';
-  } else {
-    document.getElementById('show').style.visibility = 'visible';
-  }
-});
-var proportion = '';
-async function getFriend() {
-  const friend = await liff.getFriendship();
-  return friend.friendFlag;
-}
-async function getUID() {
-  const data = await liff.getProfile();
-  const uid = await data.userId;
-  return uid;
-}
+
+//  liff.init({ liffId: '1656902981-0g1VVnpN' }).then(async () => {
+//      if (!liff.isLoggedIn()) {
+//        liff.login({
+//          redirectUri: 'https://tapp-smartcity.netlify.app/register.html'
+//        });
+//      } else if (await checkUser(await getUID())) {
+//        window.location = './index.html';
+//      } else if (!(await getFriend())) {
+//        window.location = 'https://line.me/R/ti/p/@172nwynm';
+//      } else {
+//        document.getElementById('show').style.visibility = 'visible';
+//      }
+//    });
+
+//    async function getFriend() {
+//      const friend = await liff.getFriendship();
+//      return friend.friendFlag;
+//    }
+
+//    async function getUID() {
+//      const data = await liff.getProfile();
+//      const uid = await data.userId;
+//      return uid;
+//    }
 
 
 function Disable(c) {
@@ -159,8 +161,6 @@ function set() {
   }
 }
 
-let sendData = {};
-let sum = 0;
 
 $('#TWH01').submit(function (e) {
   e.preventDefault();
@@ -173,15 +173,11 @@ $('#TWH01').submit(function (e) {
     denyButtonText: 'ยกเลิก'
   }).then(async (result) => {
     if (result.isConfirmed) {
-      var UID = await getUID();
-      sendData.userID = UID;
-          const form = $('#TWH01').serializeArray();
-          const n = form.length;
-          sum = 0;
-          form.forEach((e) => {
-            sum += Number(e.value);
-            sendData[e.name] = e.value;
-          });
+        let data = {}
+        $('#TWH01')
+          .serializeArray().forEach(e => {
+            data[e.name] =e.value
+          })
           data.BMI = $('#BMI').val();
           data.proportion = proportion;
          
@@ -190,7 +186,7 @@ $('#TWH01').submit(function (e) {
           'https://smartcity-pakpoon-api.herokuapp.com/employee/addeemployee',
           {
             method: 'POST',
-            body: JSON.stringify(sendData),
+            body: JSON.stringify(data),
             headers: {
               'Content-Type': 'application/json; charset=UTF-8'
             }
@@ -203,340 +199,10 @@ $('#TWH01').submit(function (e) {
             showCancelButton: false,
             confirmButtonText: 'ตกลง',
             timer: 3000
-          }).then(async (result) => {
-            let pop = `<div class="card">
-            <div class="card-body ">
-                <h3 class="d-flex align-items-center" style="color: blue; margin-top: 2rem; flex-direction: column;">
-                    คะแนนรวมของคุณ <span id="happyScore"></span> ${sum} คะแนน</h3>
-                <form>
-    
-                    <table id="customers">
-                        <tr>
-                            <th>คะแนนรวม</th>
-                            <th>การแปรผล</th>
-                        </tr>
-                        <tr>
-                            <td class="radio">33 - 45 คะแนน</td>
-                            <td class="radio" style="color: #006600;">มีความสุขมากกว่าคนทั่วไป (good)</td>
-    
-                        </tr>
-                        <tr>
-                            <td class="radio">27 - 32 คะแนน</td>
-                            <td class="radio" style="color: #0033FF;">มีความสุขเท่ากับคนทั่วไป (fair)</td>
-    
-                        </tr>
-                        <tr>
-                            <td class="radio">26 คะแนน หรือน้อยกว่านั้น</td>
-                            <td class="radio" style="color: #FF0000;">มีความสุขน้อยกว่าคนทั่วไป (poor)</td>
-    
-                        </tr>
-                    </table>
-                    </br>
-            </div>
-            </form>
-        </div>
-        </div>`;
-            Swal.fire({
-              html: pop,
-              showDenyButton: false,
-              showCancelButton: false,
-              confirmButtonText: 'ตกลง'
-            }).then(() => (window.location = './index.html'));
-          });
+          })
         });
       }  
 })
 })
-
-
-
-
-
-
-
-//  liff.init({ liffId: '1656902981-0g1VVnpN' }).then(async () => {
-//      if (!liff.isLoggedIn()) {
-//        liff.login({
-//          redirectUri: 'https://tapp-smartcity.netlify.app/register.html'
-//        });
-//      } else if (await checkUser(await getUID())) {
-//        window.location = './index.html';
-//      } else if (!(await getFriend())) {
-//        window.location = 'https://line.me/R/ti/p/@172nwynm';
-//      } else {
-//        document.getElementById('show').style.visibility = 'visible';
-//      }
-//    });
-
-//    async function getFriend() {
-//      const friend = await liff.getFriendship();
-//      return friend.friendFlag;
-//    }
-
-//    async function getUID() {
-//      const data = await liff.getProfile();
-//      const uid = await data.userId;
-//      return uid;
-//    }
-
-// var form = document.getElementById('form-head')
-//   form.addEventListener('submit',function(e){
-//     e.preventDefault()
-//     var prefix = document.getElementById('prefix').value
-//     var name = document.getElementById('name').value
-//     var lastname = document.getElementById('lastname').value
-
-//     fetch('https://jsonplaceholder.typicode.com/posts', {
-//       method: 'POST',
-//       body:JSON.stringify({
-//         prefix: prefix,
-//         name:name,
-//         lastname:lastname
-//       }),
-//       headers:{
-//         'content-Type': 'application/json ;charset=UTF-8'
-//       }
-//     })
-//     .then(function(response){
-//       return response.json()
-//     })
-//     .then(function(data){
-//       console.log(data)
-//     })
-//   })
-  
-
-// function Disable(c) {
-//   const a = $(`.${c}`);
-//   if ($(`.${c}`)[0].checked) {
-//     for (let i = 0; i < a.length; i++) {
-//       if (i !== 0) {
-//         a[i].disabled = true;
-//         a[i].checked = false;
-//       }
-//     }
-//     if (c === "congenital") {
-//       $(".congenital-txt").attr("disabled", true);
-//       $(".congenital-txt").val("");
-//     } else if (c === "sick") {
-//       $(".sick-txt").attr("disabled", true);
-//       $(".sick-txt").val("");
-//     } else if (c === "accident") {
-//       $(".accident-txt").attr("disabled", true);
-//       $(".accident-txt").val("");
-//     }
-//   } else {
-//     for (let i = 0; i < a.length; i++) {
-//       if (i !== 0) {
-//         a[i].disabled = false;
-//       }
-//     }
-//     if (c === "congenital") {
-//       $(".congenital-txt").attr("disabled", false);
-//     } else if (c === "sick") {
-//       $(".sick-txt").attr("disabled", false);
-//     } else if (c === "accident") {
-//       $(".accident-txt").attr("disabled", false);
-//     }
-//   }
-// }
-
-// $(".department").change((e) => {
-//   if ($(".department")[0].checked) {
-//     $("#ot").attr("disabled", false);
-//   } else {
-//     $("#ot").attr("disabled", true);
-//     $("#ot").val("");
-//   }
-// });
-
-// $(".drinking_alcohol").change((e) => {
-//   const a = $(".drinking_alcohol2");
-//   if (!$(".drinking_alcohol")[0].checked) {
-//     for (let i = 0; i < a.length; i++) {
-//       a[i].disabled = false;
-//     }
-//   } else {
-//     for (let i = 0; i < a.length; i++) {
-//       a[i].disabled = true;
-//       a[i].checked = false
-//     }
-//   }
-// });
-
-
-// $(".smoke_often1").change((e) => {
-//   if (!$(".smoke_often1")[0].checked) {
-//     $("#smoke_often1").attr("disabled", false);
-//   } else {
-//     $("#smoke_often1").attr("disabled", true);
-//     $("#smoke_often1").val("");
-//   }
-// });
-
-// $(".debt_information").change((e) => {
-//   if (!$(".debt_information")[0].checked) {
-//     $("#debt_information").attr("disabled", false);
-//   } else {
-//     $("#debt_information").attr("disabled", true);
-//     $("#debt_information").val("");
-//   }
-// });
-
-// $('#weight').change(() => {
-//   if ($('#weight').val() < 1) {
-//     $('#weight').val(1);
-//   }
-//   if (
-//     $('#weight').val() !== '' &&
-//     $('#height').val() !== '' &&
-//     $('#height').val() > 0
-//   ) {
-//     let BMI = (
-//       $('#weight').val() / Math.pow($('#height').val() / 100, 2)
-//     ).toFixed(2);
-//     $('#BMI').val(BMI);
-//     set();
-//   }
-// });
-
-// $('#height').change(() => {
-//   if ($('#height').val() < 90) {
-//     $('#height').val(90);
-//   }
-//   if (
-//     $('#weight').val() !== '' &&
-//     $('#height').val() !== '' &&
-//     $('#height').val() > 0
-//   ) {
-//     let BMI = (
-//       $('#weight').val() / Math.pow($('#height').val() / 100, 2)
-//     ).toFixed(2);
-//     $('#BMI').val(BMI);
-//     set();
-//   }
-// });
-
-// function set() {
-//   const BMI = $('#BMI').val();
-//   if (BMI < 18.5) {
-//     $('#proportion').val('น้ำหนักต่ำกว่าเกณฑ์');
-//     proportion = 'น้ำหนักต่ำกว่าเกณฑ์';
-//     $('#proportion').css('color', 'lime');
-//   } else if (BMI >= 18.5 && BMI < 22.9) {
-//     $('#proportion').val('สมส่วน');
-//     proportion = 'สมส่วน';
-//     $('#proportion').css('color', 'green');
-//   } else if (BMI >= 23 && BMI < 24.9) {
-//     $('#proportion').val('ท้วม');
-//     proportion = 'ท้วม';
-//     $('#proportion').css('color', 'yellow');
-//   } else if (BMI >= 25 && BMI < 29.9) {
-//     $('#proportion').val('โรคอ้วน');
-//     proportion = 'โรคอ้วน';
-//     $('#proportion').css('color', 'orange');
-//   } else if (BMI > 30) {
-//     $('#proportion').val('โรคอ้วนอันตราย');
-//     proportion = 'โรคอ้วนอันตราย';
-//     $('#proportion').css('color', 'red');
-//   }
-// }
-
-
-// $('#TWH01').submit(function (e) {
-//   e.preventDefault();
-//   Swal.fire({
-//     icon: 'question',
-//     title: 'ยืนยันการบันทึกข้อมูล',
-//     showDenyButton: true,
-//     showCancelButton: false,
-//     confirmButtonText: 'ยืนยัน',
-//     denyButtonText: 'ยกเลิก'
-//   }).then(async (result) => {
-//     if (result.isConfirmed) {
-//         let data = {}
-//         $('#TWH01')
-//           .serializeArray().forEach(e => {
-//             data[e.name] =e.value
-//           })
-//           data.BMI = $('#BMI').val();
-//           data.proportion = proportion;
-         
-//         console.log(data ,'data>>>');
-//         fetch(
-//           'https://smartcity-pakpoon-api.herokuapp.com/employee/addeemployee',
-//           {
-//             method: 'POST',
-//             body: JSON.stringify(data),
-//             headers: {
-//               'Content-Type': 'application/json; charset=UTF-8'
-//             }
-//           }
-//         ).then(function (response) {
-//           Swal.fire({
-//             icon: 'success',
-//             title: 'บันทึกข้อมูลเสร็จสิ้น',
-//             showDenyButton: false,
-//             showCancelButton: false,
-//             confirmButtonText: 'ตกลง',
-//             timer: 3000
-//           }).then(async (result) => {
-//             let texts = [
-//               `<tr>
-//               <td class="radio">40 - 56 คะแนน</td>
-//               <td class="radio" style="color: #FF0000;">สมมรถภาพทางสมองต่ำ ควรพบแพทย์</td>
-//           </tr>`,
-    
-//               `<tr>
-//               <td class="radio">30 - 39 คะแนน</td>
-//               <td class="radio" style="color: #FF6600 ;">สมมรถภาพทางสมองอยู่ในระดับค่อนข้างต่ำ ควรเข้ารับคำแนะนำเพื่อเพิ่มสมรรถภาพทางสมอง</td>
-//           </tr>`,
-    
-//               `<tr>
-//               <td class="radio">20 - 29 คะแนน</td>
-//               <td class="radio" style="color: #0033FF;">สมมรถภาพทางสมองอยู่ในระดับฑ์ปานกลาง ควรเข้ารับคำแนะนำเพื่อเพิ่มสมรรถภาพทางสมอง</td>
-//           </tr>`,
-//               `<tr>
-//               <td class="radio">14 - 19 คะแนน</td>
-//               <td class="radio" style="color: #006600;">สมมรถภาพทางสมองอยู่ในระดับดีมาก</td>
-//           </tr>`
-//             ];
-//             let index = () => {
-//               if (sum >= 40) {
-//                 return 0;
-//               } else if (sum >= 30) {
-//                 return 1;
-//               } else if (sum >= 20) {
-//                 return 2;
-//               } else {
-//                 return 3;
-//               }
-//             };
-//             let pop = `<div class="card">
-//             <div class="card-body ">
-//                 <h3 class="d-flex align-items-center" style="color: blue; margin-top: 2rem; flex-direction: column;">
-//                     คะแนนรวมของคุณ <span id="happyScore"></span> ${sum} คะแนน</h3>
-//                     <table id="customers">
-//                         <tr>
-//                             <th>คะแนนรวม</th>
-//                             <th>การแปรผล</th>
-//                         </tr>
-//                         ${texts[index()]}
-//                     </table>
-//                     </br>
-//             </div>
-//         </div>
-//         </div>`;
-//             Swal.fire({
-//               html: pop,
-//               showDenyButton: false,
-//               showCancelButton: false,
-//               confirmButtonText: 'ตกลง'
-//             }).then(() => (window.location = './index.html'));
-//           });
-//         });
-//       }  
-// })
-// })
 
 
