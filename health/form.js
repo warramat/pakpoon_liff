45b573397d0e338cac42aabff19b97686cd2a1d5
warramat <warramat1999@gmail.com>
2022-11-23 08:36:56 +0700

    first commit

--- health/form.js
+++ health/form.js
@@ -1,4 +1,3 @@
-
 //  liff.init({ liffId: '1656902981-0g1VVnpN' }).then(async () => {
 //      if (!liff.isLoggedIn()) {
 //        liff.login({
@@ -23,7 +22,6 @@
 //      const uid = await data.userId;
 //      return uid;
 //    }
-
 
 function Disable(c) {
   const a = $(`.${c}`);
@@ -78,11 +76,10 @@ $(".drinking_alcohol").change((e) => {
   } else {
     for (let i = 0; i < a.length; i++) {
       a[i].disabled = true;
-      a[i].checked = false
+      a[i].checked = false;
     }
   }
 });
-
 
 $(".smoke_often1").change((e) => {
   if (!$(".smoke_often1")[0].checked) {
@@ -102,228 +99,216 @@ $(".debt_information").change((e) => {
   }
 });
 
-$('#weight').change(() => {
-  if ($('#weight').val() < 1) {
-    $('#weight').val(1);
+$("#weight").change(() => {
+  if ($("#weight").val() < 1) {
+    $("#weight").val(1);
   }
   if (
-    $('#weight').val() !== '' &&
-    $('#height').val() !== '' &&
-    $('#height').val() > 0
+    $("#weight").val() !== "" &&
+    $("#height").val() !== "" &&
+    $("#height").val() > 0
   ) {
     let BMI = (
-      $('#weight').val() / Math.pow($('#height').val() / 100, 2)
+      $("#weight").val() / Math.pow($("#height").val() / 100, 2)
     ).toFixed(2);
-    $('#BMI').val(BMI);
+    $("#BMI").val(BMI);
     set();
   }
 });
 
-$('#height').change(() => {
-  if ($('#height').val() < 90) {
-    $('#height').val(90);
+$("#height").change(() => {
+  if ($("#height").val() < 90) {
+    $("#height").val(90);
   }
   if (
-    $('#weight').val() !== '' &&
-    $('#height').val() !== '' &&
-    $('#height').val() > 0
+    $("#weight").val() !== "" &&
+    $("#height").val() !== "" &&
+    $("#height").val() > 0
   ) {
     let BMI = (
-      $('#weight').val() / Math.pow($('#height').val() / 100, 2)
+      $("#weight").val() / Math.pow($("#height").val() / 100, 2)
     ).toFixed(2);
-    $('#BMI').val(BMI);
+    $("#BMI").val(BMI);
     set();
   }
 });
 
 function set() {
-  const BMI = $('#BMI').val();
+  const BMI = $("#BMI").val();
   if (BMI < 18.5) {
-    $('#proportion').val('น้ำหนักต่ำกว่าเกณฑ์');
-    proportion = 'น้ำหนักต่ำกว่าเกณฑ์';
-    $('#proportion').css('color', 'lime');
+    $("#proportion").val("น้ำหนักต่ำกว่าเกณฑ์");
+    proportion = "น้ำหนักต่ำกว่าเกณฑ์";
+    $("#proportion").css("color", "lime");
   } else if (BMI >= 18.5 && BMI < 22.9) {
-    $('#proportion').val('สมส่วน');
-    proportion = 'สมส่วน';
-    $('#proportion').css('color', 'green');
+    $("#proportion").val("สมส่วน");
+    proportion = "สมส่วน";
+    $("#proportion").css("color", "green");
   } else if (BMI >= 23 && BMI < 24.9) {
-    $('#proportion').val('ท้วม');
-    proportion = 'ท้วม';
-    $('#proportion').css('color', 'yellow');
+    $("#proportion").val("ท้วม");
+    proportion = "ท้วม";
+    $("#proportion").css("color", "yellow");
   } else if (BMI >= 25 && BMI < 29.9) {
-    $('#proportion').val('โรคอ้วน');
-    proportion = 'โรคอ้วน';
-    $('#proportion').css('color', 'orange');
+    $("#proportion").val("โรคอ้วน");
+    proportion = "โรคอ้วน";
+    $("#proportion").css("color", "orange");
   } else if (BMI > 30) {
-    $('#proportion').val('โรคอ้วนอันตราย');
-    proportion = 'โรคอ้วนอันตราย';
-    $('#proportion').css('color', 'red');
+    $("#proportion").val("โรคอ้วนอันตราย");
+    proportion = "โรคอ้วนอันตราย";
+    $("#proportion").css("color", "red");
   }
 }
 
-
-$('#TWH01').submit(function (e) {
-  let isNumber =['age']
+$("#TWH01").submit(function (e) {
+  let isNumber = ["age"];
   e.preventDefault();
   Swal.fire({
-    icon: 'question',
-    title: 'ยืนยันการบันทึกข้อมูล',
+    icon: "question",
+    title: "ยืนยันการบันทึกข้อมูล",
     showDenyButton: true,
     showCancelButton: false,
-    confirmButtonText: 'ยืนยัน',
-    denyButtonText: 'ยกเลิก'
+    confirmButtonText: "ยืนยัน",
+    denyButtonText: "ยกเลิก",
   }).then(async (result) => {
     if (result.isConfirmed) {
-        let data = {}
-        let sumStrain =0
-        let sumHappy = 0
-        let sumMemory =0
-        $('#TWH01')
-          .serializeArray().forEach(e => {
-            if(isNumber.find(k=> k==e.name)){
-              data[e.name] = Number(e.value)
-            }else{
-              data[e.name] =e.value
-            }
-            
-          })
-          for(let i=1;i<=20;i++){
-            sumStrain += Number(data[`Strain${i}`])
+      let data = {};
+      let sumStrain = 0;
+      let sumHappy = 0;
+      let sumMemory = 0;
+      $("#TWH01")
+        .serializeArray()
+        .forEach((e) => {
+          if (isNumber.find((k) => k == e.name)) {
+            data[e.name] = Number(e.value);
+          } else {
+            data[e.name] = e.value;
           }
-          for(let i=1;i<=15;i++){
-            sumHappy += Number(data[`happy${i}`])
-          }
-          for(let i=1;i<=14;i++){
-            sumMemory += Number(data[`memory${i}`])
-          }
-          data.BMI = Number ($('#BMI').val());
-          data.sumStrain =sumStrain
-          data.sumHappy = sumHappy
-          data.sumMemory = sumMemory
-          data.proportion = proportion;
-        fetch(
-          'https://smartcity-pakpoon-api.herokuapp.com/employee/addeemployee',
-          {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-              'Content-Type': 'application/json; charset=UTF-8'
-            }
-          }
-        ).then(function (response) {
+        });
+      for (let i = 1; i <= 20; i++) {
+        sumStrain += Number(data[`Strain${i}`]);
+      }
+      for (let i = 1; i <= 15; i++) {
+        sumHappy += Number(data[`happy${i}`]);
+      }
+      for (let i = 1; i <= 14; i++) {
+        sumMemory += Number(data[`memory${i}`]);
+      }
+      data.BMI = Number($("#BMI").val());
+      data.sumStrain = sumStrain;
+      data.sumHappy = sumHappy;
+      data.sumMemory = sumMemory;
+      data.proportion = proportion;
+      fetch(
+        "https://smartcity-pakpoon-api.herokuapp.com/employee/addeemployee",
+        {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+          },
+        }
+      ).then(function (response) {
+        Swal.fire({
+          icon: "success",
+          title: "บันทึกข้อมูลเสร็จสิ้น",
+          showDenyButton: false,
+          showCancelButton: false,
+          confirmButtonText: "ตกลง",
+          timer: 3000,
+        }).then((e) => {
+          let pop = `<div class="card">
+                        <div class="card-body ">
+                            <h3 class="d-flex align-items-center" style="color: blue; margin-top: 2rem; flex-direction: column;">
+                                คะแนนความเครียด ${sumStrain} คะแนน</h3>
+                
+                                <table id="customers">
+                                    <tr>
+                                        <th>คะแนนรวม</th>
+                                        <th>การแปรผล</th>
+                                    </tr>
+                                    <tr>
+                                        <td class="radio">0 - 23 คะแนน</td>
+                                        <td class="radio" style="color: #006600;">มีความเครียดในระดับน้อยและหายไปได้ในระยะเวลาสั้นๆ</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="radio">24 - 41 คะแนน</td>
+                                        <td class="radio" style="color: #0033FF;">มีความเครียดในระดับปานกลาง</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="radio">42 - 61 คะแนน</td>
+                                        <td class="radio" style="color: #FF6600;">มีความเครียดในระดับสูง</td>
+                                  </tr>
+                                    <tr>
+                                        <td class="radio">62 คะแนนขึ้นไป</td>
+                                        <td class="radio" style="color: #FF0000;">มีความเครียดในระดับรุนแรง</td>
+                                  </tr>
+                                </table>
+                        </div>
+                     </div>
+                <br>
+                      <div class="card">
+                          <div class="card-body">
+                              <h3 class="d-flex align-items-center" style="color: blue; margin-top: 2rem; flex-direction: column;">
+                                  คะแนนความสุข  ${sumHappy} คะแนน</h3>
+                                  <table id="customers">
+                                      <tr>
+                                          <th>คะแนนรวม</th>
+                                          <th>การแปรผล</th>
+                                      </tr>
+                                      <tr>
+                                          <td class="radio">33 - 45 คะแนน</td>
+                                          <td class="radio" style="color: #006600;">มีความสุขมากกว่าคนทั่วไป (good)</td>
+                                      </tr>
+                                      <tr>
+                                          <td class="radio">27 - 32 คะแนน</td>
+                                          <td class="radio" style="color: #0033FF;">มีความสุขเท่ากับคนทั่วไป (fair)</td>
+                                      </tr>
+                                      <tr>
+                                          <td class="radio">26 คะแนน หรือน้อยกว่านั้น</td>
+                                          <td class="radio" style="color: #FF0000;">มีความสุขน้อยกว่าคนทั่วไป (poor)</td>
+                                      </tr>
+                                  </table>
+                          </div>
+                      </div>
+                 <br>
+                      <div class="card">
+                        <div class="card-body">
+                            <h3 class="d-flex align-items-center" style="color: blue; margin-top: 2rem; flex-direction: column;">
+                                คะแนนความจำ  ${sumMemory} คะแนน</h3>
+                
+                                <table id="customers">
+                                    <tr>
+                                        <th>คะแนนรวม</th>
+                                        <th>การแปรผล</th>
+                                    </tr>
+                                    <tr>
+                                        <td class="radio">40 - 56 คะแนน</td>
+                                        <td class="radio" style="color: #FF0000;">สมมรถภาพทางสมองต่ำ ควรพบแพทย์</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="radio">30 - 39 คะแนน</td>
+                                        <td class="radio" style="color: #FF6600 ;">สมมรถภาพทางสมองอยู่ในระดับค่อนข้างต่ำ ควรเข้ารับคำแนะนำเพื่อเพิ่มสมรรถภาพทางสมอง</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="radio">20 - 29 คะแนน</td>
+                                        <td class="radio" style="color: #0033FF;">สมมรถภาพทางสมองอยู่ในระดับฑ์ปานกลาง ควรเข้ารับคำแนะนำเพื่อเพิ่มสมรรถภาพทางสมอง</td>
+                                    </tr>
+                                    <tr>
+                                        <td class="radio">14 - 19 คะแนน</td>
+                                        <td class="radio" style="color: #006600;">สมมรถภาพทางสมองอยู่ในระดับดีมาก</td>
+                                    </tr>
+                                </table>
+                        </div>
+                    </div>`;
           Swal.fire({
-            icon: 'success',
-            title: 'บันทึกข้อมูลเสร็จสิ้น',
+            html: pop,
             showDenyButton: false,
             showCancelButton: false,
-            confirmButtonText: 'ตกลง',
-            timer: 3000
-          }).then((e)=>{
-            let pop = `<div class="card">
-            <div class="card-body ">
-                <h3 class="d-flex align-items-center" style="color: blue; margin-top: 2rem; flex-direction: column;">
-                    คะแนนรวมของคุณ  ${sumStrain} คะแนน</h3>
-    
-                    <table id="customers">
-                        <tr>
-                            <th>คะแนนรวม</th>
-                            <th>การแปรผล</th>
-                        </tr>
-                        <tr>
-                            <td class="radio">33 - 45 คะแนน</td>
-                            <td class="radio" style="color: #006600;">มีความสุขมากกว่าคนทั่วไป (good)</td>
-    
-                        </tr>
-                        <tr>
-                            <td class="radio">27 - 32 คะแนน</td>
-                            <td class="radio" style="color: #0033FF;">มีความสุขเท่ากับคนทั่วไป (fair)</td>
-    
-                        </tr>
-                        <tr>
-                            <td class="radio">26 คะแนน หรือน้อยกว่านั้น</td>
-                            <td class="radio" style="color: #FF0000;">มีความสุขน้อยกว่าคนทั่วไป (poor)</td>
-    
-                        </tr>
-                    </table>
-                    </br>
-            </div>
-        </div>
-        </div>
-        <br>
-        <div class="card">
-            <div class="card-body ">
-                <h3 class="d-flex align-items-center" style="color: blue; margin-top: 2rem; flex-direction: column;">
-                    คะแนนรวมของคุณ  ${sumHappy} คะแนน</h3>
-    
-                    <table id="customers">
-                        <tr>
-                            <th>คะแนนรวม</th>
-                            <th>การแปรผล</th>
-                        </tr>
-                        <tr>
-                            <td class="radio">33 - 45 คะแนน</td>
-                            <td class="radio" style="color: #006600;">มีความสุขมากกว่าคนทั่วไป (good)</td>
-    
-                        </tr>
-                        <tr>
-                            <td class="radio">27 - 32 คะแนน</td>
-                            <td class="radio" style="color: #0033FF;">มีความสุขเท่ากับคนทั่วไป (fair)</td>
-    
-                        </tr>
-                        <tr>
-                            <td class="radio">26 คะแนน หรือน้อยกว่านั้น</td>
-                            <td class="radio" style="color: #FF0000;">มีความสุขน้อยกว่าคนทั่วไป (poor)</td>
-    
-                        </tr>
-                    </table>
-                    </br>
-            </div>
-        </div>
-        </div>
-    
-        <br>
-        <div class="card">
-            <div class="card-body ">
-                <h3 class="d-flex align-items-center" style="color: blue; margin-top: 2rem; flex-direction: column;">
-                    คะแนนรวมของคุณ  ${sumMemory} คะแนน</h3>
-    
-                    <table id="customers">
-                        <tr>
-                            <th>คะแนนรวม</th>
-                            <th>การแปรผล</th>
-                        </tr>
-                        <tr>
-                            <td class="radio">33 - 45 คะแนน</td>
-                            <td class="radio" style="color: #006600;">มีความสุขมากกว่าคนทั่วไป (good)</td>
-    
-                        </tr>
-                        <tr>
-                            <td class="radio">27 - 32 คะแนน</td>
-                            <td class="radio" style="color: #0033FF;">มีความสุขเท่ากับคนทั่วไป (fair)</td>
-    
-                        </tr>
-                        <tr>
-                            <td class="radio">26 คะแนน หรือน้อยกว่านั้น</td>
-                            <td class="radio" style="color: #FF0000;">มีความสุขน้อยกว่าคนทั่วไป (poor)</td>
-    
-                        </tr>
-                    </table>
-                    </br>
-            </div>
-        </div>
-        </div>`;
-            Swal.fire({
-              html: pop,
-              showDenyButton: false,
-              showCancelButton: false,
-              confirmButtonText: 'ตกลง'
-            }).then(e=>{
-              location.reload()
-            })
-          })
+            confirmButtonText: "ตกลง",
+          }).then((e) => {
+            location.reload();
+          });
         });
-      }  
-})
-})
-
-
+      });
+    }
+  });
+});
